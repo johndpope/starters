0d10b28d6c785f8a100bfad447669b1d7fb54af3
Noorvir Aulakh <noorvir.aulakh@gmail.com>
2022-08-25 14:40:44 -0700

    Fix download script to use only native packages

--- mnist/download.py
+++ mnist/download.py
@@ -1,9 +1,46 @@
-from torchvision import datasets
+import os
+import gzip
+import shutil
+from turtle import down
+import urllib.request
+
+base = "http://yann.lecun.com/exdb/mnist/"
+
+out_dir = "uwstore/data/MNIST/raw/"
+download_dir = "tmp/"
+
+files = [
+    "train-images-idx3-ubyte.gz",
+    "train-labels-idx1-ubyte.gz", 
+    "t10k-images-idx3-ubyte.gz",
+    "t10k-labels-idx1-ubyte.gz",
+]
+
+
+if not os.path.exists(download_dir):
+    os.makedirs(download_dir)
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
 
 
 def main():
-    dataset1 = datasets.MNIST('./uwstore/data', train=True, download=True)
-    dataset2 = datasets.MNIST('./uwstore/data', train=False, download=True)
+    for f in files:
+        url = base + f
+        download_path = download_dir + f
+        out_path = out_dir + f[:-3]
+        
+        print("Downloading file: ", f)
+        with urllib.request.urlopen(url) as response, open(download_path, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+
+        with gzip.open(download_path, 'rb') as f_in:
+            with open(out_path, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+    print("Finished downloading %d files" % len(files))
+    shutil.rmtree(download_dir)
+
 
 if __name__ == '__main__':
     main()
